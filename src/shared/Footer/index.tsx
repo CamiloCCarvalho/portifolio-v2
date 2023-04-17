80545476bbcdb49b230416af5fadc2cdb94910ee
Camilo Costa <asfaltorodas@gmail.com>
2023-04-17 13:49:20 -0300

    Commit Att

--- src/shared/Footer/index.tsx
+++ src/shared/Footer/index.tsx
@@ -20,7 +20,7 @@ const Footer:React.FC = () => {
                 </div>
 
                 <div className="card-footer">
-                   Atualizado: 16/04/2023 ---  Versão: 2.0.82
+                   Atualizado: 17/04/2023 ---  Versão: 2.0.95
                 </div>
             </FooterBox>
     )
