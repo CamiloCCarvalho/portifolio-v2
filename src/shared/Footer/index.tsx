42dc2c6bd66c6e485c610f20a73d6c5f8dc7b921
Camilo <asfaltorodas@gmail.com>
2023-07-22 00:22:27 -0300

    att

--- src/shared/Footer/index.tsx
+++ src/shared/Footer/index.tsx
@@ -20,7 +20,7 @@ const Footer:React.FC = () => {
                 </div>
 
                 <div className="card-footer">
-                   Atualizado: 15/07/2023 ---  Versão: 2.1.27
+                   Atualizado: 15/07/2023 ---  Versão: 2.1.34
                 </div>
             </FooterBox>
     )
